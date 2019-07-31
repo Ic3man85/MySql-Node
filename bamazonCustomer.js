fd0d83450125a0f7b9dacaff6ca8217e2e2c9a3f
Iceman <Zachary.Draper85@gmail.com>
2019-07-31 17:57:41 -0600

    working on the prompts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,12 +12,30 @@ let con = mysql.createConnection({
 con.connect(function(err) {
     if (err) throw err;
     console.log("CONNECTED!!!" + con.threadId);
-    con.query("SELECT * FROM products", function(err, result) {
-        let data = result.length;
+    con.query("SELECT * FROM products", function(err, result, fields) {
         if (err) throw err;
-        for (let i = 0; i < data; i++) {
-            console.log("Id: " + data.products.item_id);
+        // console.log(result[2].item_id);
+        for (let i = 0; i < result.length; i++) {
+            console.log("Item:" + result[i].item_id + " " + result[i].product_name + " Price:" + result[i].price);
         }
+        promptUser();
+        con.end();
     })
-    con.end();
 });
+
+function promptUser() {
+    inquirer.prompt([{
+            type: "input",
+            name: "item",
+            message: "Choose an item number!"
+        },
+        {
+            type: "input",
+            name: "numberItems",
+            message: "How many would you like?"
+
+        }
+    ]).then(function(result) {
+        console.log(result);
+    });
+}
